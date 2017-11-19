ee028e36fd418e167e4ff839706da81f85f48ac6
Dev <coolthug@hotmail.fr>
2017-11-20 00:14:10 +0100

    Test d'envoi de trames OK

--- client.py
+++ client.py
@@ -7,8 +7,8 @@ from ctypes import *
 class MntFrameHeader(LittleEndianStructure):
     _pack_ = 1
     _fields_ = [
-        ("source_address", c_uint8),
         ("dest_address", c_uint8),
+        ("source_address", c_uint8),
         ("frame_param", c_uint32),
         ("command", c_uint8),
         ("data_size", c_uint16),
@@ -19,20 +19,16 @@ class MntFrame(LittleEndianStructure):
     _pack_ = 1
     _fields_ = [
         ("header", MntFrameHeader),
-        ("data", c_uint8 * 1024),
+        ("data", c_uint8 * (1000 - sizeof(MntFrameHeader))),
     ]
 
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+TCP_IP = '192.168.0.12'
+TCP_PORT = 1000
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 mess = create_string_buffer(0x30, 1024)
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((TCP_IP, TCP_PORT))
-# s.send(MESSAGE)
-# data = s.recv(BUFFER_SIZE)
-# s.close()
+
 test = c_uint16 * 5
 
 frame = MntFrame()
@@ -45,8 +41,15 @@ print("adresse de ptData : " + hex(addressof(ptData)))
 print("adresse de frame : " + hex(addressof(frame)))
 print("adresse de frame.data : " + hex(addressof(frame.data)))
 
+frame.header.dest_address = 2
+frame.header.source_address = 170
+frame.data[0] = 0x0C
+frame.data[1] = 0xED
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(frame)
+data = s.recv(BUFFER_SIZE)
+s.close()
 
 
 pass
-
-
