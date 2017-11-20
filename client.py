a28e7c223aab42b2cf86222ea85b65ca8e38dfe7
Dev <coolthug@hotmail.fr>
2017-11-20 19:31:36 +0100

    * Création de la structure LedControl -> Test OK

--- client.py
+++ client.py
@@ -32,35 +32,64 @@ class MntFrame(LittleEndianStructure):
     ]
 
 
+class PWMControl(LittleEndianStructure):
+    _pack_ = 1
+    _fields_ = [
+        ("frequency", c_uint16),
+        ("dutyCycle", c_uint8)
+    ]
+
+
+class LedControlType(Union):
+    _pack_ = 1
+    _fields_ = [
+        ("normal", c_uint8),
+        ("pwm", PWMControl)
+    ]
+
+
+class LedControlFrame(LittleEndianStructure):
+    _pack_ = 1
+    _fields_ = [
+        ("header", MntFrameHeader),
+        ("ledNumber", c_uint8),
+        ("ledControlType", c_uint8),
+        ("type", LedControlType)
+    ]
+
 TCP_IP = '127.0.0.1'
-TCP_PORT = 55502
+TCP_IP = '192.168.0.12'
+TCP_PORT = 1000
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!"
 mess = create_string_buffer(0x30, 1024)
 
 test = c_uint16 * 5
 
-frame = MntFrame()
-ptData = pointer(frame.data)
-param = MntFrameParam()
-param.isParted = 3
-print("taille de MntFrame : " + str(sizeof(MntFrame)))
-print("taille de c_char_p : " + str(sizeof(c_char_p)))
-print("Valeur de ptData : " + str(param.isParted))
-print("taille de MntFrameParam : " + str(sizeof(param)))
-print("adresse de ptData : " + hex(addressof(ptData)))
-print("adresse de frame : " + hex(addressof(frame)))
-print("adresse de frame.data : " + hex(addressof(frame.data)))
+frame = LedControlFrame()
+
+#print("taille de MntFrame : " + str(sizeof(MntFrame)))
+#print("taille de c_char_p : " + str(sizeof(c_char_p)))
+#print("Valeur de ptData : " + str(param.isParted))
+#print("taille de MntFrameParam : " + str(sizeof(param)))
+#print("adresse de ptData : " + hex(addressof(ptData)))
+#print("adresse de frame : " + hex(addressof(frame)))
+#print("adresse de frame.data : " + hex(addressof(frame.data)))
 
 frame.header.dest_address = 2
-frame.header.source_address = 0xAA
-frame.header.frame_param.isParted = 3
-frame.data[0] = 0x0C
-frame.data[1] = 0xED
+frame.header.source_address = 1
+frame.header.command = 1
+frame.header.data_size = sizeof(LedControlFrame) - sizeof(MntFrameHeader)
+
+frame.ledNumber = 0
+frame.ledControlType = 0
+frame.type.normal = 0
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.send(frame)
 data = s.recv(BUFFER_SIZE)
+# ret = cast(data, LedControlFrame)
 s.close()
 
 
